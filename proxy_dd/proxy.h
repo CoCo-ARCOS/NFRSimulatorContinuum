1e707a5348b7a031110a4a937f95db8f327a162a
ddomizzisg <ddomizzisag@gmail.com>
2026-05-19 13:13:46 -0600

    error reduced

--- proxy_dd/proxy.h
+++ proxy_dd/proxy.h
@@ -78,7 +78,6 @@ struct stage_definition
 	double b_fs_write; /* filesystem write bandwidth bytes/sec for this stage */
 	double application_mean_service_time; /* average application execution time in seconds */
 	double application_size_factor; /* multiplicative change in object size caused by the application */
-	double application_size_factor; /* multiplicative change in object size caused by the application */
 };
 
 struct machine_node
@@ -86,9 +85,6 @@ struct machine_node
 	char name[64];
 	int stages[10];
 	int stages_number;
-	char hardware_profile[64];
-	char real_values_dir[512];
-	int service_profile_index;
 	char hardware_profile[64];
 	char real_values_dir[512];
 	int service_profile_index;
@@ -117,7 +113,6 @@ struct worker
 	int stage;
 	int stage_owner;		 /* Current stage this worker batch is assigned to (1..10). */
 	int machine_id;			 /* Assigned machine index, -1 if local/not set */
-	int service_profile_index; /* Loaded interpolation profile for this worker's active machine. */
 	int service_profile_index; /* Loaded interpolation profile for this worker's active machine. */
 	char agent_type[16];	 /**< "output" or "input" */
 	int pipeline_is_input;	 /**< Current pipeline: 1=input/acquisition, 0=output/delivery. */
